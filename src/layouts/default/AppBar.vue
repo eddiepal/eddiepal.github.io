d0632137b83d7e5e3750ab5c31fbc222e2870eab
Eddie <empburke@outlook.com>
2023-02-19 16:07:59 +0000

    Added WIP texts

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -5,15 +5,38 @@
           <span class="title text-uppercase mx-4">Edward Burke</span>
       </router-link>
     </v-app-bar-title>
-    <v-divider vertical></v-divider>
-      <v-btn variant="text" class="mx-2">About</v-btn>
-    <v-divider vertical></v-divider>
-      <v-btn variant="text" class="mx-2">Projects</v-btn>
-    <v-divider vertical></v-divider>
-      <v-btn variant="text" class="mx-2">Other</v-btn>
-    <v-divider vertical class="mr-2"></v-divider>
+      <v-divider vertical></v-divider>
+      <div class="wip-wrapper">
+        <v-btn variant="text" class="wip-wrapper" disabled>About</v-btn>
+        <div variant="text" class="wip-text">WIP</div>
+      </div>
+      <v-divider vertical></v-divider>
+      <div class="wip-wrapper">
+        <v-btn variant="text" class="mx-2" disabled>Projects</v-btn>
+        <div variant="text" class="wip-text">WIP</div>
+      </div>
+      <v-divider vertical></v-divider>
+      <div class="wip-wrapper">
+        <v-btn variant="text" class="mx-2" disabled>Other</v-btn>
+        <div variant="text" class="wip-text">WIP</div>
+      </div>
+      <v-divider vertical class="mr-2"></v-divider>
   </v-app-bar>
 </template>
+
+<style>
+  .wip-wrapper {
+    position: relative;
+  }
+  .wip-text {
+    position: absolute;
+    padding-top: 25px;
+    color: red;
+    position: absolute;
+    top: 0%;
+    left: 35%;
+  }
+</style>
 
 <script lang="ts" setup>
   //
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,81 @@
 <template>
-  <HelloWorld />
+<v-container class="fill-height">
+    <v-responsive class="d-flex align-center text-center fill-height">
+      <!-- <v-img
+        contain
+        height="300"
+        src="@/assets/logo.svg"
+      /> -->
+
+      <div class="text-body-2 font-weight-light mb-n1"></div>
+
+      <h1 class="text-h4 font-weight-bold mt-10">This website is a WIP - 
+        for now go check out Minecraft Legends which has absolutely 
+        nothing to do with this website other than the fact that I am VERY excited for it!!
+        And I want others to be too!</h1>
+      <h2 class="text-h5 font-weight-bold"><a href="https://www.minecraft.net/en-us/about-legends">Minecraft Legends</a></h2>
+      <div class="py-14" />
+
+      <!-- <v-row class="d-flex align-center justify-center">
+        <v-col cols="auto">
+          <v-btn
+            href="https://next.vuetifyjs.com/components/all/"
+            min-width="164"
+            rel="noopener noreferrer"
+            target="_blank"
+            variant="text"
+          >
+            <v-icon
+              icon="mdi-view-dashboard"
+              size="large"
+              start
+            />
+
+            Components
+          </v-btn>
+        </v-col>
+
+        <v-col cols="auto">
+          <v-btn
+            color="primary"
+            href="https://next.vuetifyjs.com/introduction/why-vuetify/#feature-guides"
+            min-width="228"
+            rel="noopener noreferrer"
+            size="x-large"
+            target="_blank"
+            variant="flat"
+          >
+            <v-icon
+              icon="mdi-speedometer"
+              size="large"
+              start
+            />
+
+            Get Started
+          </v-btn>
+        </v-col>
+
+        <v-col cols="auto">
+          <v-btn
+            href="https://community.vuetifyjs.com/"
+            min-width="164"
+            rel="noopener noreferrer"
+            target="_blank"
+            variant="text"
+          >
+            <v-icon
+              icon="mdi-account-group"
+              size="large"
+              start
+            />
+
+            Community
+          </v-btn>
+        </v-col>
+      </v-row> -->
+    </v-responsive>
+  </v-container>
+
 </template>
 
 <script lang="ts" setup>
