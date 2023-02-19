2fd68573b25e43927fb3dd73bb47efa6dcfc5a8f
Eddie <empburke@outlook.com>
2023-02-19 12:10:11 +0000

    Set up app bar

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,10 +1,17 @@
 <template>
-  <v-app-bar flat>
+  <v-app-bar :elevation="2">
     <v-app-bar-title>
-      <v-icon icon="mdi-circle-slice-6" />
-
-      Essentials Preset
+      <router-link to="/" style="text-decoration: none; color: inherit;">
+          <span class="title text-uppercase mx-4">Edward Burke</span>
+      </router-link>
     </v-app-bar-title>
+    <v-divider vertical></v-divider>
+      <v-btn variant="text" class="mx-2">About</v-btn>
+    <v-divider vertical></v-divider>
+      <v-btn variant="text" class="mx-2">Projects</v-btn>
+    <v-divider vertical></v-divider>
+      <v-btn variant="text" class="mx-2">Other</v-btn>
+    <v-divider vertical class="mr-2"></v-divider>
   </v-app-bar>
 </template>
 
