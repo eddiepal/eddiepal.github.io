d26ca0de23e8e2b329bf4619c706e027cd04d7a1
Eddie <empburke@outlook.com>
2023-07-22 15:45:00 +0100

    New iteration (with issues to fix) of the site & removed the app bar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,80 +1,362 @@
 <template>
-<v-container class="fill-height">
-    <v-responsive class="d-flex align-center text-center fill-height">
-      <!-- <v-img
-        contain
-        height="300"
-        src="@/assets/logo.svg"
-      /> -->
-
-      <div class="text-body-2 font-weight-light mb-n1"></div>
-
-      <h1 class="text-h4 font-weight-bold mt-10">This website is a WIP by me (Eddie) as of July 2023. Come back later to see changes.</h1>
-      <h1 class="text-h4 font-weight-bold mt-10">Thanks for visiting!</h1>
-      <div class="py-14" />
-
-      <!-- <v-row class="d-flex align-center justify-center">
-        <v-col cols="auto">
-          <v-btn
-            href="https://next.vuetifyjs.com/components/all/"
-            min-width="164"
-            rel="noopener noreferrer"
-            target="_blank"
-            variant="text"
+  <div style="background-color: #b2daff">
+    <div id="fullNameHeader">EDWARD BURKE</div>
+    <div id="personalOverviewHeader">Tester. Coder. Quality Engineer.</div>
+    <div style="height: auto; position: relative; margin: auto">
+      <div id="contentBackground">
+        <div
+          style="width: 95%; height: auto; top: 20px; position: relative; left: 20px; right: 20px color: black; font-size: 20px; font-family: Inter; font-weight: 400;"
+        >
+          Hello! I am Quality Assurance Engineer with a focus on the automation
+          of testing. I love software that is delivered with the quality that
+          exceeds user’s expectations. That is always the aim! Whether it is the
+          direct user experience, security of the software, regulatory
+          requirements or whatever else. I have a passion for these requirements
+          to be met or if possible, met beyond what is required in a way that
+          positively impacts the end user!
+        </div>
+        <div class="sectionContent">
+          <div class="sectionHeaders">WORK EXPERIENCE</div>
+          <div class="informationHeader">
+            <span
+              style="
+                color: black;
+                font-size: 20px;
+                font-family: Inter;
+                font-weight: 700;
+                word-wrap: break-word;
+              "
+              >QA Automation Engineer <br /></span
+            ><span
+              style="
+                color: black;
+                font-size: 20px;
+                font-family: Inter;
+                font-weight: 400;
+                word-wrap: break-word;
+              "
+              >Engage XR: September 2021 - Current Day</span
+            >
+          </div>
+          <ul class="commonUnorderedList">
+            <li>
+              Planning, creating, and running of various types of auto & manual
+              tests
+            </li>
+            <li>
+              Test automation using Cypress together with JavaScript & Cucumber
+            </li>
+            <li>Creation of automation tools</li>
+            <li>Writing automated Frontend E2E tests</li>
+            <li>Writing automated API tests</li>
+            <li>
+              Triages, sprint reviews, communication with developers and POs as
+              required
+            </li>
+            <li>In depth document reviewing and verification</li>
+            <li>Manual testing where required</li>
+          </ul>
+          <div class="informationHeader">
+            <span
+              style="
+                color: black;
+                font-size: 20px;
+                font-family: Inter;
+                font-weight: 700;
+                word-wrap: break-word;
+              "
+              >Student Unity Developer<br /></span
+            ><span
+              style="
+                color: black;
+                font-size: 20px;
+                font-family: Inter;
+                font-weight: 400;
+                word-wrap: break-word;
+              "
+              >Virtual Rehab Platform (V3R): January – June 2019</span
+            >
+          </div>
+          <div class="informationHeader">
+            <span
+              style="
+                color: black;
+                font-size: 20px;
+                font-family: Inter;
+                font-weight: 700;
+                word-wrap: break-word;
+              "
+              >Funpark Assistant<br /></span
+            ><span
+              style="
+                color: black;
+                font-size: 20px;
+                font-family: Inter;
+                font-weight: 400;
+                word-wrap: break-word;
+              "
+              >Courtown Funpark: Summer 2017 and 2018</span
+            >
+          </div>
+          <div
+            style="
+              position: relative;
+              color: black;
+              font-size: 20px;
+              font-family: Inter;
+              font-weight: 400;
+              word-wrap: break-word;
+            "
           >
-            <v-icon
-              icon="mdi-view-dashboard"
-              size="large"
-              start
-            />
-
-            Components
-          </v-btn>
-        </v-col>
-
-        <v-col cols="auto">
-          <v-btn
-            color="primary"
-            href="https://next.vuetifyjs.com/introduction/why-vuetify/#feature-guides"
-            min-width="228"
-            rel="noopener noreferrer"
-            size="x-large"
-            target="_blank"
-            variant="flat"
-          >
-            <v-icon
-              icon="mdi-speedometer"
-              size="large"
-              start
-            />
-
-            Get Started
-          </v-btn>
-        </v-col>
-
-        <v-col cols="auto">
-          <v-btn
-            href="https://community.vuetifyjs.com/"
-            min-width="164"
-            rel="noopener noreferrer"
-            target="_blank"
-            variant="text"
-          >
-            <v-icon
-              icon="mdi-account-group"
-              size="large"
-              start
-            />
-
-            Community
-          </v-btn>
-        </v-col>
-      </v-row> -->
-    </v-responsive>
-  </v-container>
-
+            <ul class="commonUnorderedList">
+              <li>
+                Developer for VR project using HTC Vive and Leap Motion sensor
+              </li>
+              <li>Working with Unity & C#</li>
+              <li>Worked in a team of four students</li>
+              <li>
+                1st place award received for the project at Windesheim
+                University
+              </li>
+            </ul>
+          </div>
+          <div
+            style="width: 414.77px; left: 0px; top: 572px; position: relative"
+          ></div>
+        </div>
+        <div class="sectionContent">
+          <div class="sectionHeaders">EDUCATION</div>
+          <div>
+            <span
+              style="
+                color: black;
+                font-size: 20px;
+                font-family: Inter;
+                font-weight: 700;
+                word-wrap: break-word;
+              "
+              >BSc (Hons) in Applied Computing (Games Development)<br /></span
+            ><span
+              style="
+                color: black;
+                font-size: 20px;
+                font-family: Inter;
+                font-weight: 400;
+                word-wrap: break-word;
+              "
+              >Waterford Institute of Technology (Ireland) <br />2016 - 2020<br /><br /></span
+            ><span
+              style="
+                color: black;
+                font-size: 20px;
+                font-family: Inter;
+                font-weight: 700;
+                word-wrap: break-word;
+              "
+              >Erasmus Advanced BSc minor (Hons) in Games Programming<br /></span
+            ><span
+              style="
+                color: black;
+                font-size: 20px;
+                font-family: Inter;
+                font-weight: 400;
+                word-wrap: break-word;
+              "
+              >Windesheim University of Applied Sciences (Zwolle,
+              Netherlands)<br />January - July 2019<br /><br /></span
+            ><span
+              style="
+                color: black;
+                font-size: 20px;
+                font-family: Inter;
+                font-weight: 700;
+                word-wrap: break-word;
+              "
+              >Graphic Design – PLC FETAC Level 5 Award<br /></span
+            ><span
+              style="
+                color: black;
+                font-size: 20px;
+                font-family: Inter;
+                font-weight: 400;
+                word-wrap: break-word;
+              "
+              >Selskar College (Westgate, Wexford) <br />2015 – 2016<br
+            /></span>
+          </div>
+        </div>
+        <div class="sectionContent">
+          <div class="sectionHeaders">CERTIFICATIONS & ACHIEVEMENTS</div>
+          <div>
+            <ul class="commonUnorderedList">
+              <li>ISTQB CTFL certified</li>
+              <li>
+                Award winner as a team for the Winnovation 2019 Games category
+                of Windesheim University
+              </li>
+              <li>Completed the Erasmus Programme in the Netherlands</li>
+              <li>Volunteer for Waterford Global Game Jam 2018</li>
+            </ul>
+          </div>
+        </div>
+        <div class="sectionContent">
+          <div class="sectionHeaders">CONTACT</div>
+          <div>
+            <span
+              style="
+                color: black;
+                font-size: 20px;
+                font-family: Inter;
+                font-weight: 400;
+                word-wrap: break-word;
+              "
+              >Best way to contact me is through </span
+            ><span
+              style="
+                color: black;
+                font-size: 20px;
+                font-family: Inter;
+                font-weight: 400;
+                text-decoration: underline;
+                word-wrap: break-word;
+              "
+              >my profile</span
+            ><span
+              style="
+                color: black;
+                font-size: 20px;
+                font-family: Inter;
+                font-weight: 400;
+                word-wrap: break-word;
+              "
+            >
+              on LinkedIn.</span
+            >
+          </div>
+        </div>
+      </div>
+      <div
+        style="
+          width: 1760px;
+          height: 0px;
+          left: 1419px;
+          top: 0px;
+          position: relative;
+          transform: rotate(90deg);
+          transform-origin: 0 0;
+          border: 0.5px black solid;
+        "
+      ></div>
+      <div
+        style="
+          width: 1760px;
+          height: 0px;
+          left: 0px;
+          top: 58.75px;
+          position: relative;
+          transform: rotate(90deg);
+          transform-origin: 0 0;
+          border: 0.5px black solid;
+        "
+      ></div>
+    </div>
+  </div>
 </template>
 
+<style>
+#namePageTitle {
+  width: 475px;
+  flex-shrink: 0;
+  color: #000;
+  text-align: center;
+  font-family: Inter;
+  font-size: 40px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+}
+
+#fullNameHeader {
+  margin: auto;
+  width: 50%;
+  height: 10px;
+  text-align: center;
+  color: black;
+  font-size: 40px;
+  font-family: Inter;
+  font-weight: 800;
+  position: center;
+  border-bottom: 2px solid black;
+  padding-bottom: 70px;
+  border-right-width: 300;
+  border-left-width: 200px;
+  margin-bottom: 50px;
+  padding-top: 30px;
+}
+
+#personalOverviewHeader {
+  height: 80px;
+  left: 281.21px;
+  top: 0px;
+  text-align: center;
+  color: black;
+  font-size: 40px;
+  font-family: Inter;
+  font-weight: 700;
+  word-wrap: break-word;
+  margin: auto;
+}
+
+#contentBackground {
+  width: 1151.88px;
+  height: auto;
+  left: 133.5px;
+  width: 60%;
+  position: center;
+  background: #bfe0ff;
+  border-radius: 31px;
+  margin: auto;
+  text-align: left;
+}
+
+.informationHeader {
+  line-height: 30px;
+  margin-top: 20px;
+}
+
+.sectionHeaders {
+  text-align: center;
+  color: black;
+  font-size: 24px;
+  font-family: Inter;
+  font-weight: 800;
+  text-decoration: underline;
+  word-wrap: break-word;
+  margin: auto;
+  margin-top: 50px;
+  margin-bottom: 10px;
+  position: relative;
+}
+
+.sectionContent {
+  position: relative;
+  padding-left: 35px;
+  height: auto;
+}
+
+.commonUnorderedList {
+  width: 1110.2px;
+  position: relative;
+  color: black;
+  font-size: 20px;
+  font-family: Inter;
+  font-weight: 400;
+  word-wrap: break-word;
+  margin-bottom: 25px;
+  left: 20px;
+}
+</style>
+
 <script lang="ts" setup>
-  import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 </script>
